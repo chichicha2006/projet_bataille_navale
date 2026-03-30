966f407c4d5efe6251e30311f3f45057371e5347
AICHA TAHARBELLAR <aichataharbellar2006@gmail.com>
2026-03-30 13:28:47 +0200

    grille 26x26 au lieu de 26x40

--- src/plateau/plateau.py
+++ src/plateau/plateau.py
@@ -14,7 +14,7 @@ class ResultatDeplacement(Enum):
 
 class Plateau:
     NB_LIGNES = 26
-    NB_COLONNES = 40
+    NB_COLONNES = 26
 
     def __init__(self, x_loc=55, y_loc=160, cell_width=16):
         self.x_loc = x_loc
